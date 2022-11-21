091d024c35144f87acd532772a8b0a8f7ffb5f67
james-muriithi <muriithijames556@gmail.com>
2022-11-21 11:40:04 +0300

    [js] remove duplicates from sorted array

---
+++ javascript/remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,18 @@
+/**
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+ *  time complexity O(n)
+ *  space complexity O(n)
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var removeDuplicates = function(nums) {
+    const countNums = {}
+    for(let i = 0; i < nums.length;) {
+        countNums[nums[i]] = (countNums[nums[i]] || 0) + 1;
+        if(countNums[nums[i]] > 2) {
+            nums.splice(i, 1)
+            continue;
+        }
+        i++
+    }
+};
